b67bc0110331c33dc6c0a1854667414d5b66fe6a
VinczeDan <vincze.daniel.22f@szlgbp.hu>
2025-03-13 20:31:45 +0100

    egy csapat kesz

--- script.js
+++ script.js
@@ -35,3 +35,12 @@ document.querySelectorAll('.dropdown').forEach(function(dropdown) {
         dropdownMenu.classList.toggle('active');
     });
 });
+
+window.addEventListener("scroll", function() {
+    const header = document.querySelector("header");
+    if (window.scrollY > 50) { // Adjust the scroll threshold as needed
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
